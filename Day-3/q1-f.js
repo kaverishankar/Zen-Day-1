93e46b233274c7f6b3e0899306f2b41db3f65e1c
Kaveri shankar M <71606190+kaverishankar@users.noreply.github.com>
2024-02-19 16:15:29 +0530

    Update q1-f.js

--- Day-3/q1-f.js
+++ Day-3/q1-f.js
@@ -1,12 +1,15 @@
 // Return median of two sorted arrays of the same size //
 
-let arr1 = [1,2,3,4];
-let arr2 = [5,6,7,8];
+let arr1 = [1,2,3,7];
+let arr2 = [5,6,4,8];
 
 // Anonymous funcion
 let median = function (num1,num2)
 {
-    let sum=num1[num1.length-1]+num2[0];
+    let arr = arr1.concat(arr2);
+    arr.sort();
+    let mid = arr.length/2;
+    let sum=arr[mid]+arr[mid-1];
     let avg=sum/2;
     console.log(avg);
 }
@@ -16,7 +19,10 @@ let median = function (num1,num2)
  // IIFE function
 (function (num1,num2)
 {
-    let sum=num1[num1.length-1]+num2[0];
+    let arr = arr1.concat(arr2);
+    arr.sort();
+    let mid = arr.length/2;
+    let sum=arr[mid]+arr[mid-1];
     let avg=sum/2;
     console.log(avg);
 })(arr1,arr2);
