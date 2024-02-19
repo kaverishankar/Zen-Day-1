2e8d74ac44367b131bdbaa3fca24490ac3b54d34
Kaveri shankar M <71606190+kaverishankar@users.noreply.github.com>
2024-02-19 16:28:03 +0530

    Update q1-f.js

--- Day-3/q1-f.js
+++ Day-3/q1-f.js
@@ -1,13 +1,22 @@
 // Return median of two sorted arrays of the same size //
 
 let arr1 = [1,2,3,7];
-let arr2 = [5,6,4,8];
+let arr2 = [10,6,4,8];
 
 // Anonymous funcion
 let median = function (num1,num2)
 {
     let arr = arr1.concat(arr2);
-    arr.sort();
+    for (let i = 0; i < arr.length; i++) 
+    { 
+    for (let j = i+1; j < arr.length; j++) 
+    { if(arr[i] > arr[j]) { 
+                 let temp = arr[i]; 
+                 arr[i] = arr[j]; 
+                 arr[j] = temp; 
+              } 
+           } 
+     } 
     let mid = arr.length/2;
     let sum=arr[mid]+arr[mid-1];
     let avg=sum/2;
@@ -20,7 +29,16 @@ let median = function (num1,num2)
 (function (num1,num2)
 {
     let arr = arr1.concat(arr2);
-    arr.sort();
+    for (let i = 0; i < arr.length; i++) 
+    { 
+    for (let j = i+1; j < arr.length; j++) 
+    { if(arr[i] > arr[j]) { 
+                 let temp = arr[i]; 
+                 arr[i] = arr[j]; 
+                 arr[j] = temp; 
+              } 
+           } 
+     }
     let mid = arr.length/2;
     let sum=arr[mid]+arr[mid-1];
     let avg=sum/2;
